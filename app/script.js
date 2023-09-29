ee6b16f335a6b9ec5c8404a6b4089e36e73ced07
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-09-29 11:53:43 +0530

    added funtion to showload

--- app/script.js
+++ app/script.js
@@ -1,42 +1,78 @@
-getAllDogsFromApi();
-function getAllDogsFromApi() {
-  // https://dog.ceo/api/breeds/list/all
-  const url = "https://dog.ceo/api/breeds/list/all";
-  $.ajax(url, {
-    method: "GET",
-    success: function (resp) {
-      console.log("Api request success");
-      const dogsList = Object.keys(resp.message);
-      console.log(dogsList);
-      appendToSelect(dogsList);
-    },
-    error: function () {
-      console.log("Api request error");
-    },
-    complete: function () {
-      console.log("API request completed");
-    },
-  });
-}
-function getRandomImageOfDog(dogBreed) {
-  // https://dog.ceo/api/breed/affenpinscher/images/random
-  const url = `https://dog.ceo/api/breed/${dogBreed}/images/random`;
-  $.ajax(url, {
-    method: "GET",
-    success: function (resp) {
-      console.log("Api request success");
-      console.log(resp);
-    },
-    error: function () {
-      console.log("Api request error");
-    },
-  });
-}
-function appendToSelect(list) {
-  for (let item of list) {
-    let i = 1;
-    const option = document.createElement("option");
-    option.innerHTML = item;
-    option.id = item + i;
+$(document).ready(function () {
+  const loadingIndicator = $("#loadingIndicator");
+
+  function showLoading() {
+    loadingIndicator.show();
   }
-}
+
+  function hideLoading() {
+    loadingIndicator.hide();
+  }
+
+  function populateDropdown(dogBreeds) {
+    const dropdown = $("#Dropdown");
+
+    dropdown.append('<option value="">Select a breed</option>');
+
+    for (let i = 0; i < dogBreeds.length; i++) {
+      const breed = dogBreeds[i];
+      dropdown.append(`<option value="${breed}">${breed}</option>`);
+    }
+  }
+
+  $("#button").click(function () {
+    const selectedBreed = $("#Dropdown").val();
+    if (selectedBreed) {
+      getRandomImageOfDog(selectedBreed);
+    }
+  });
+
+  function getAllDogsFromApi() {
+    const url = "https://dog.ceo/api/breeds/list/all";
+    $.ajax(url, {
+      method: "GET",
+      success: function (resp) {
+        console.log("API request success");
+        const dogsList = Object.keys(resp.message);
+        console.log(dogsList);
+        populateDropdown(dogsList);
+      },
+      error: function () {
+        console.log("API request error");
+      },
+      complete: function () {
+        console.log("API request completed");
+      },
+    });
+  }
+
+  function getRandomImageOfDog(dogBreed) {
+    const url = `https://dog.ceo/api/breed/${dogBreed}/images/random`;
+    showLoading();
+    $.ajax(url, {
+      method: "GET",
+      success: function (resp) {
+        console.log("API request success");
+        console.log(resp);
+        hideLoading();
+        displayImage(resp.message);
+      },
+      error: function () {
+        console.log("API request error");
+      },
+    });
+  }
+
+  function displayImage(imageUrl) {
+    const imageContainer = $("#imageContainer");
+
+    imageContainer.empty();
+
+    const imgElement = $("<img>");
+    imgElement.attr("src", imageUrl);
+
+    imageContainer.append(imgElement);
+  }
+
+  getAllDogsFromApi();
+});
